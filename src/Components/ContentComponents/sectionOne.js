4a04cfe742ff4f1a2f71e5ee0a56cfaf3be55bf8
Spencer Lindemuth <root@SpencerSurfacePro.localdomain>
2019-10-10 15:27:42 -0700

    added more technologies

--- src/Components/ContentComponents/sectionOne.js
+++ src/Components/ContentComponents/sectionOne.js
@@ -16,7 +16,7 @@ export default class sectionOne extends React.Component{
                 description: "A website designed to teach CSS basics to people of all ages in an interactive learning environment",
                 url: "https://CSSchool.herokuapp.com",
                 gitUrl: "https://github.com/spencerlindemuth/CSSchool-frontend",
-                tech: [{name: "JavaScript", use: "Dynamically add CSS styles on button click to Window stylesheet and general navigation"}, {name: "React", use: "Full front-end architecture creating single page application and render lessons"}, {name: "Ruby On Rails", use: "Restful API serves lesson components, including html and css to be applied and rendered on page load"}, {name: "JWT Authentication", use: "Securely login user accounts and verify accounts to save user progress"}, {name: "React-Markdown", use: "Renders plain text as HTML elements"}, {name: "CSS", use: "Site-wide styling and mobile responsiveness"}, {name: "HTML", use: "Site Structure and lesson formatting"}]
+                tech: [{name: "JavaScript", use: "Dynamically add CSS styles on button click to Window stylesheet and general navigation"}, {name: "React", use: "Full front-end architecture creating single page application and render lessons"}, {name: "Ruby On Rails", use: "Restful API serves lesson components, including html and css to be applied and rendered on page load"}, {name: "JWT token based Authentication", use: "Securely login user accounts and verify accounts to save user progress"}, {name: "React-Markdown", use: "Renders plain text as HTML elements"}, {name: "CSS", use: "Site-wide styling and mobile responsiveness"}, {name: "HTML", use: "Site Structure and lesson formatting"}, {name: "PostgreSQL", use: "Store lesson text, templates, button text, HTML elements in plain-text, button action CSS, and any predefined CSS rules"}, {name: "Heroku", use: "Dual Dynos hosting React front-end as well as Rails API"}]
             },
             {
                 id: 2,
@@ -24,7 +24,8 @@ export default class sectionOne extends React.Component{
                 image: hilo,
                 description: "A turn based, two player game based on the popular card game Acey Deucey",
                 url: "http://hi-lo.netlify.com",
-                gitUrl: "https://github.com/marth00165/HILO-front-end"
+                gitUrl: "https://github.com/marth00165/HILO-front-end",
+                tech: [{name: "JavaScript", use: "Created vanilla JavaScript single page application, all game logic, game flow, effects, and animations"}, {name: "Ruby on Rails", use: "Restful API endpoint for serving and storing names and high scores"} , {name: "CSS", use: "Pure CSS for full site styling and mobile responsiveness"}, {name: "HTML", use: "Site structure"}, {name: "Heroku", use: "Deployment space for Rails API"}, {name: "PostgreSQL", use: "Store usernames and high scores"}, {name: "Netlify", use: "Hosting JavaScript front end"}]
 
             },
             {
@@ -33,7 +34,8 @@ export default class sectionOne extends React.Component{
                 image: artsyfartsy,
                 description: "A website that allows users to create their own digital gallery of select works from the METTM museum",
                 url: "https://ArtsyFartsy.herokuapp.com",
-                gitUrl: "https://github.com/SpencerLindemuth/ArtsyFartsy-Frontend"
+                gitUrl: "https://github.com/SpencerLindemuth/ArtsyFartsy-Frontend",
+                tech: [{name: "React", use: "Create dynamic single page application showcasing users gallery as well as pieces and piece show pages"}, {name: "Ruby on Rails", use: "Serve artworks from the MET open access API using RESTful API endpoints as well as filter pieces and serve users owned pieces"}, {name: "PostgreSQL", use: "Store user pieces and all public domain works in the MET open access program"}, {name: "JWT token based authentication", use: "Securely log users in and limit access to API nodes"}, {name: "Heroku", use: "Host Ruby API and React front-end"}, {name: "CSS", use: "Pure CSS used for all site elements and styling"}, {name: "React Draggable", use: "Drag pieces and place them on users gallery wall, combined with browser localstorage to record location and persist gallery layout"}, {name: "HTML", use: "Site structure"}, {name: "JavaScript", use: "Handle API requests and handles filtering of Artworks in browse page"}]
 
             }],
 
--- src/Components/ViewComponents/projectCard.js
+++ src/Components/ViewComponents/projectCard.js
@@ -6,17 +6,17 @@ let ProjectCard = (props) => {
             <h1>{props.title}</h1>
             <img onClick={() => window.location.href=props.url} src={props.background}/>
     <p>{props.description} &nbsp; {props.expandedBool ? <span onClick={() => props.closeClick()} className="more">Close</span> : <span onClick={() => props.moreClick(props.id)} className="more">More &#8594;</span>}</p>
-            {props.technology ? <h2>Technologies</h2> : null}
+            {props.technology ? <h1>Technologies</h1> : null}
             <ul className="technologyList">
                 {props.technology ? props.technology.map(tech => {
                     return( 
                         <li>
-                            <h6>{tech.name}</h6>
-                            <p>{tech.use}</p>
+                            <h2>{tech.name}</h2>
+                            <p className="tech-use">{tech.use}</p>
                         </li>
                 )}) : null}
             </ul>
-            <a href={props.url}>Website</a><a href={props.gitUrl}>Github</a>
+            <a href={props.url}>Website</a><a href={props.gitUrl}>Github</a>{props.expandedBool ? <a id="close-link" onClick={props.closeClick}>Close</a> : null }
         </div>
     )
 }
